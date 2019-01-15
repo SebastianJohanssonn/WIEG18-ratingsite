3118ac716858467cd97eb210f8ebb1f2173c6d8c
Sebastian Johansson <sebben.97@hotmail.com>
2019-01-15 15:14:28 +0100

    fixed names and descriptions for the new array.

--- functions.php
+++ functions.php
@@ -2,17 +2,17 @@
 function sortList(){
     include "list.php";
 
-    foreach(array_keys($movies) as $key){
-        echo "<h4><option>". $key . "</option></h4>";
+    foreach($movies as $key){
+        echo "<h4><option>". $key['name'] . "</option></h4>";
     }
 }
 function getDescription(){
     include "list.php";
     if(isset($_POST['submit'])){
         $selected = $_POST['dropdown'];
-        foreach($movies as $key => $value){
-            if($key == $selected){
-                echo $value;
+        foreach($movies as $key){
+            if($selected == $key['name']){
+                echo $key['description'];
             }
         }
     }
